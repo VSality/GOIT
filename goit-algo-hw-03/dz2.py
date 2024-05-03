0cf5b7355fc2adc455b4e78dceafdd46ce180b32
VSality <118682593+VSality@users.noreply.github.com>
2024-05-03 23:15:03 +0300

    Goit algo hw 03 (#3)
    * add dz1 dz2
    
    * save

---
+++ goit-algo-hw-03/dz2.py
@@ -0,0 +1,30 @@
+import turtle
+
+def koch_curve(t, order, size):
+    if order == 0:
+        t.forward(size)
+    else:
+        for angle in [60, -120, 60, 0]:
+            koch_curve(t, order - 1, size / 3)
+            t.left(angle)
+
+def snowflake(t, order, size):
+    for _ in range(3):
+        koch_curve(t, order, size)
+        t.right(120)            
+
+def draw_koch_curve(order, size=300):
+    window = turtle.Screen()
+    window.bgcolor("white")
+
+    t = turtle.Turtle()
+    t.speed(0)  
+    t.penup()
+    t.goto(-size / 2, 0)
+    t.pendown()
+
+    snowflake(t, order, size)
+
+    window.mainloop()
+
+draw_koch_curve(3)
