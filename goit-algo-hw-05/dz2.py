432de0bae25522bf4aa20830c8e221faa8863b3f
VSality <118682593+VSality@users.noreply.github.com>
2024-05-11 20:47:48 +0300

    Goit algo hw 05 (#5)
    * add dz1 dz2
    
    * save
    
    * dz4
    
    * save
    
    * save

---
+++ goit-algo-hw-05/dz2.py
@@ -0,0 +1,35 @@
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    last = 0
+    iter = 0
+ 
+    while low <= high:
+ 
+        mid = (high + low) // 2
+        iter += 1
+ 
+        # якщо x більше за значення посередині списку, ігноруємо ліву половину
+        if arr[mid] < x:
+            low = mid + 1
+            last = low
+ 
+        # якщо x менше за значення посередині списку, ігноруємо праву половину
+        elif arr[mid] > x:
+            high = mid - 1
+            
+        # інакше x присутній на позиції і повертаємо його
+        else:
+            return [iter, mid]
+
+    if last >= len(arr):
+        return [iter, -1]
+    # якщо елемент не знайдений
+    return [iter, last]
+
+arr = [2.2, 3.1, 4.3, 5.2, 6.2, 7, 8.3, 9.1]
+x = 7
+result = binary_search(arr, x)
+print(result)
+
---
+++ goit-algo-hw-05/dz3.py
@@ -0,0 +1,60 @@
+import timeit
+from knut_search import kmp_search
+from boer_search import boyer_moore_search
+from rabin_search import rabin_karp_search
+
+
+# Открываем файл и читаем его содержимое
+with open('goit-algo-hw-05/page1.txt', 'r', encoding='windows-1251') as file:
+    page1_string = file.read()
+    
+with open('goit-algo-hw-05/page2.txt', 'r', encoding='windows-1251') as file:
+    page2_string = file.read()
+
+
+pattern_yes_page1 = "бібліотеках"
+pattern_no_page1 = "блабла"
+
+pattern_yes_page2 = "даних"
+pattern_no_page2 = "блабла"
+
+print("----- Knut -------------")
+execution_time = timeit.timeit(f'{kmp_search(page1_string, pattern_yes_page1)}', number=1000000)
+print("Время выполнения YES Knut page1:", execution_time, "секунд", "найдено ", kmp_search(page1_string, pattern_yes_page1))
+execution_time = timeit.timeit(f'{kmp_search(page1_string, pattern_no_page1)}', number=1000000)
+print("Время выполнения NO Knut page1:", execution_time, "секунд", "найдено ", kmp_search(page1_string, pattern_no_page1))
+
+execution_time = timeit.timeit(f'{kmp_search(page2_string, pattern_yes_page2)}', number=1000000)
+print("Время выполнения YES Knut page2:", execution_time, "секунд", "найдено ", kmp_search(page2_string, pattern_yes_page2))
+execution_time = timeit.timeit(f'{kmp_search(page2_string, pattern_no_page2)}', number=1000000)
+print("Время выполнения NO Knut page2:", execution_time, "секунд", "найдено ", kmp_search(page2_string, pattern_no_page2))
+
+print("----- Boer -------------")
+execution_time = timeit.timeit(f'{boyer_moore_search(page1_string, pattern_yes_page1)}', number=1000000)
+print("Время выполнения YES Boer page1:", execution_time, "секунд", "найдено ", boyer_moore_search(page1_string, pattern_yes_page1))
+execution_time = timeit.timeit(f'{boyer_moore_search(page1_string, pattern_no_page1)}', number=1000000)
+print("Время выполнения NO Boer page1:", execution_time, "секунд", "найдено ", boyer_moore_search(page1_string, pattern_no_page1))
+
+execution_time = timeit.timeit(f'{boyer_moore_search(page2_string, pattern_yes_page2)}', number=1000000)
+print("Время выполнения YES Boer page2:", execution_time, "секунд", "найдено ", boyer_moore_search(page2_string, pattern_yes_page2))
+execution_time = timeit.timeit(f'{boyer_moore_search(page2_string, pattern_no_page2)}', number=1000000)
+print("Время выполнения NO Boer page2:", execution_time, "секунд", "найдено ", boyer_moore_search(page2_string, pattern_no_page2))
+
+print("----- Rabin -------------")
+execution_time = timeit.timeit(f'{rabin_karp_search(page1_string, pattern_yes_page1)}', number=1000000)
+print("Время выполнения YES Rabin page1:", execution_time, "секунд", "найдено ", rabin_karp_search(page1_string, pattern_yes_page1))
+execution_time = timeit.timeit(f'{rabin_karp_search(page1_string, pattern_no_page1)}', number=1000000)
+print("Время выполнения NO Rabin page1:", execution_time, "секунд", "найдено ", rabin_karp_search(page1_string, pattern_no_page1))
+
+execution_time = timeit.timeit(f'{rabin_karp_search(page2_string, pattern_yes_page2)}', number=1000000)
+print("Время выполнения YES Rabin page2:", execution_time, "секунд", "найдено ", rabin_karp_search(page2_string, pattern_yes_page2))
+execution_time = timeit.timeit(f'{rabin_karp_search(page2_string, pattern_no_page2)}', number=1000000)
+print("Время выполнения NO Rabin page2:", execution_time, "секунд", "найдено ", rabin_karp_search(page2_string, pattern_no_page2))
+
+# ВИСНОВОК:
+
+# У нас є два тексти, на кожний ми взяли слово для пошуку (паттерн) яке існує в ціх текстах, та другий паттерн якого точно неіснує
+# Усі три алгоритми працюють +- однаково швидко, є невелика різниця в часі між пошуком патерну який існує і якого неіснує.
+# Можливо ми побачили різницю на більш великих текстах
+
+
